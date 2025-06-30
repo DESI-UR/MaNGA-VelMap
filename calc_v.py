f34cf31886e24089d4e6057b52a328629ba94306
Emlilio <benitezemilio19@gmail.com>
2025-06-30 10:58:56 -0400

    calculated the rotation velocity at each given radius in an array (commented)

--- calc_v.py
+++ calc_v.py
@@ -4,11 +4,54 @@
 # In[ ]:
 
 import numpy.ma as ma
-
 import numpy as np
 
-def calc_v(r_array,params  ):
+def calc_v(r_array,params):
+    '''
+    Function
+    ^^^^^^^^
+    Calculate the velocity at each radius
+    =====================================
+
+    Parameters
+    ^^^^^^^^^^
+
+    params : length-7 list or array
+    model parameters in the following order:
     
+        vmax : Quantity object
+            maximum velocity (km/s)
+
+        alpha : float
+            sharpness of the galaxy rotation curve
+
+        Rturn : float
+            radius where the rotation curve changes from rising to flat
+
+        PA : float
+            position angle of the galaxy (radians)
+
+        i_angle : float
+            inclination angle of the galaxy (radians)
+
+        clean_coords_i : int
+            i index of the galaxy center in pixels
+
+        clean_coords_j : int
+            j index of the galaxy center in pixels
+
+    r_array : array
+
+    radius array with deprojected length
+
+    ====================================
+
+    Return
+    ^^^^^^
+
+    v : array
+    the rotational velocity at each radius
+    '''
     vmax = params[0]
     Rturn = params[1]
     alpha = params[2]
